826943487485fcdb4c9252b222a60d8c13f8bcde
idindrakusuma <indrakusuma.udinus@gmail.com>
2017-09-12 09:48:27 +0700

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+* VOTE APP - Laravel Version
+* See Codeigniter Version : https://github.com/creativefls/voteApp
+* Author : @idindrakusuma | www.idindrakusuma.web.id
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
